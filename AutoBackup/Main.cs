aa10142e629de01f303d5e7fd117dada33274015
Najoan <68422289+najoan125@users.noreply.github.com>
2023-09-28 13:33:54 +0900

    무언가가 바뀜

--- AutoBackup/Main.cs
+++ AutoBackup/Main.cs
@@ -1,14 +1,12 @@
 ﻿using HarmonyLib;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using UnityEngine;
 using UnityModManagerNet;
+using Application = UnityEngine.Application;
 
 namespace AutoBackup
 {
@@ -37,23 +35,7 @@ namespace AutoBackup
                 harmony = new Harmony(modEntry.Info.Id);
                 harmony.PatchAll(Assembly.GetExecutingAssembly());
 
-                string userPath = Path.Combine("User");
-                string start = userPath + @"\시작";
-                DirectoryInfo startdi = new DirectoryInfo(start);
-                if (!startdi.Exists)
-                {
-                    startdi.Create();
-                }
-                System.IO.File.Copy(userPath + @"\data.sav", userPath + @"\시작\" + System.DateTime.Now.ToString("yyyy-MM-dd tt hh.mm.ss") + ".sav");
-
-                string userPath_Custom = Path.Combine("User");
-                string start_Custom = userPath_Custom + @"\시작_Custom";
-                DirectoryInfo startdi_Custom = new DirectoryInfo(start_Custom);
-                if (!startdi_Custom.Exists)
-                {
-                    startdi_Custom.Create();
-                }
-                System.IO.File.Copy(userPath_Custom + @"\custom_data.sav", userPath_Custom + @"\시작_Custom\" + System.DateTime.Now.ToString("yyyy-MM-dd tt hh.mm.ss") + ".sav");
+                CreateBackupFile("시작");
             }
             else
             {
@@ -65,94 +47,96 @@ namespace AutoBackup
 
         private static void OnGUI(UnityModManager.ModEntry modEntry)
         {
-            if (GUILayout.Button("백업 폴더 열기"))
+            if (GUILayout.Button("백업 폴더 열기", GUILayout.Width(200)))
             {
                 string userPath = Path.Combine("User");
-                System.Diagnostics.Process.Start(userPath);
+                Process.Start(userPath);
             }
             GUILayout.Label(" ");
-            if (GUILayout.Button("데이터를 Adofai를 시작할 때 백업된 파일로 복원하기"))
+            GUILayout.Label("사용자 설정 및 공식맵 데이터");
+            if (GUILayout.Button("Adofai를 시작할 때 백업된 파일로 복원하기", GUILayout.Width(300)))
             {
-                string userPath = Path.Combine("User", "시작");
-                string user = Path.Combine("User");
-                OpenFileDialog dialog = new OpenFileDialog();
-                dialog.InitialDirectory = userPath;
-                dialog.Filter = "sav files (*.sav)|*.sav";
-                dialog.FilterIndex = 2;
-                dialog.RestoreDirectory = true;
-                if (dialog.ShowDialog() == DialogResult.OK)
-                {
-                    System.IO.File.Copy(dialog.FileName, user + @"\data.sav", true);
-                }
+                RestoreData("시작");
             }
 
-            if (GUILayout.Button("데이터를 Adofai를 종료할 때 백업된 파일로 복원하기"))
+            if (GUILayout.Button("Adofai를 종료할 때 백업된 파일로 복원하기", GUILayout.Width(300)))
             {
-                string userPath = Path.Combine("User", "종료");
-                string user = Path.Combine("User");
-                OpenFileDialog dialog = new OpenFileDialog();
-                dialog.InitialDirectory = userPath;
-                dialog.Filter = "sav files (*.sav)|*.sav";
-                dialog.FilterIndex = 2;
-                dialog.RestoreDirectory = true;
-                if (dialog.ShowDialog() == DialogResult.OK)
-                {
-                    System.IO.File.Copy(dialog.FileName, user + @"\data.sav", true);
-                }
+                RestoreData("종료");
             }
 
             GUILayout.Label(" ");
+            GUILayout.Label("커스텀 레벨");
 
-            if (GUILayout.Button("커스텀 레벨 데이터를 Adofai를 시작할 때 백업된 파일로 복원하기"))
+            if (GUILayout.Button("Adofai를 시작할 때 백업된 파일로 복원하기", GUILayout.Width(300)))
             {
-                string userPath = Path.Combine("User", "시작_Custom");
-                string user = Path.Combine("User");
-                OpenFileDialog dialog = new OpenFileDialog();
-                dialog.InitialDirectory = userPath;
-                dialog.Filter = "sav files (*.sav)|*.sav";
-                dialog.FilterIndex = 2;
-                dialog.RestoreDirectory = true;
-                if (dialog.ShowDialog() == DialogResult.OK)
-                {
-                    System.IO.File.Copy(dialog.FileName, user + @"\custom_data.sav", true);
-                }
+                RestoreCustomData("시작");
             }
-            if (GUILayout.Button("커스텀 레벨 데이터를 Adofai를 종료할 때 백업된 파일로 복원하기"))
+            if (GUILayout.Button("Adofai를 종료할 때 백업된 파일로 복원하기", GUILayout.Width(300)))
             {
-                string userPath = Path.Combine("User", "종료_Custom");
-                string user = Path.Combine("User");
-                OpenFileDialog dialog = new OpenFileDialog();
-                dialog.InitialDirectory = userPath;
-                dialog.Filter = "sav files (*.sav)|*.sav";
-                dialog.FilterIndex = 2;
-                dialog.RestoreDirectory = true;
-                if (dialog.ShowDialog() == DialogResult.OK)
-                {
-                    System.IO.File.Copy(dialog.FileName, user + @"\custom_data.sav", true);
-                }
+                RestoreCustomData("종료");
             }
-            GUILayout.Label("(데이터 복원은 Adofai를 재시작 해야 적용됩니다)");
+            GUILayout.Label(" ");
+            GUILayout.Label("(백업 파일을 불러오는 순간 ADOFAI가 재시작됩니다!)");
         }
 
         private static void OnSaveGUI(UnityModManager.ModEntry modEntry)
         {
+            CreateBackupFile("종료");
+        }
+
+        private static void CreateBackupFile(String name)
+        {
             string userPath = Path.Combine("User");
-            string start = userPath + @"\종료";
+            string start = userPath + @"\"+name;
             DirectoryInfo startdi = new DirectoryInfo(start);
             if (!startdi.Exists)
             {
                 startdi.Create();
             }
-            System.IO.File.Copy(userPath + @"\data.sav", userPath + @"\종료\" + System.DateTime.Now.ToString("yyyy-MM-dd tt hh.mm.ss") + ".sav");
+            File.Copy(userPath + @"\data.sav", userPath + @"\"+name+@"\" + DateTime.Now.ToString("yyyy-MM-dd tt hh.mm.ss") + ".sav");
 
             string userPath_Custom = Path.Combine("User");
-            string start_Custom = userPath_Custom + @"\종료_Custom";
+            string start_Custom = userPath_Custom + @"\"+name+"_Custom";
             DirectoryInfo startdi_Custom = new DirectoryInfo(start_Custom);
             if (!startdi_Custom.Exists)
             {
                 startdi_Custom.Create();
             }
-            System.IO.File.Copy(userPath_Custom + @"\custom_data.sav", userPath_Custom + @"\종료_Custom\" + System.DateTime.Now.ToString("yyyy-MM-dd tt hh.mm.ss") + ".sav");
+            File.Copy(userPath_Custom + @"\custom_data.sav", userPath_Custom + @"\"+name+@"_Custom\" + DateTime.Now.ToString("yyyy-MM-dd tt hh.mm.ss") + ".sav");
+        }
+
+        private static void RestoreData(String name)
+        {
+            string userPath = Path.Combine("User", name);
+            string user = Path.Combine("User");
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = userPath;
+            dialog.Filter = "sav files (*.sav)|*.sav";
+            dialog.FilterIndex = 2;
+            dialog.RestoreDirectory = true;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                File.Copy(dialog.FileName, user + @"\data.sav", true);
+                Process.Start(Application.dataPath.Replace("_Data", ".exe"));
+                Application.Quit();
+            }
+        }
+
+        private static void RestoreCustomData(String name)
+        {
+            string userPath = Path.Combine("User", name+"_Custom");
+            string user = Path.Combine("User");
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = userPath;
+            dialog.Filter = "sav files (*.sav)|*.sav";
+            dialog.FilterIndex = 2;
+            dialog.RestoreDirectory = true;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                File.Copy(dialog.FileName, user + @"\custom_data.sav", true);
+                Process.Start(Application.dataPath.Replace("_Data", ".exe"));
+                Application.Quit();
+            }
         }
     }
 }
